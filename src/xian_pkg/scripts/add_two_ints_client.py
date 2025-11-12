2203375012c248243cc0ae5637c43aa0f4b81855
lishixian <714492291@qq.com>
2025-11-12 09:14:10 +0800

    feat: 添加sservice的python代码demo

---
+++ src/xian_pkg/scripts/add_two_ints_client.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import rospy
+from xian_pkg.srv import AddTwoInts
+
+def add_two_ints_client(a, b):
+    rospy.wait_for_service('add_two_ints')
+    try:
+        add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
+        resp = add_two_ints(a, b)
+        return resp.sum
+    except rospy.ServiceException as e:
+        rospy.logerr("服务调用失败: %s", e)
+
+if __name__ == "__main__":
+    a, b = 1, 2
+    rospy.init_node('add_two_ints_client')
+    rospy.loginfo("请求 %d + %d", a, b)
+    print("结果:", add_two_ints_client(a, b))
---
+++ src/xian_pkg/scripts/add_two_ints_server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import rospy
+from xian_pkg.srv import AddTwoInts, AddTwoIntsResponse
+
+def handle_add_two_ints(req):
+    rospy.loginfo("收到请求: a=%d, b=%d", req.a, req.b)
+    result = req.a + req.b
+    rospy.loginfo("响应结果: sum=%d", result)
+    return AddTwoIntsResponse(result)
+
+def add_two_ints_server():
+    rospy.init_node('add_two_ints_server')
+    service = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
+    rospy.loginfo("已准备好相加两个整数（Python 服务）。")
+    rospy.spin()
+
+if __name__ == "__main__":
+    add_two_ints_server()
